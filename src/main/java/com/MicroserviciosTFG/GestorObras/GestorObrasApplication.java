dc654ba24e8cd58101a2e8e41ed2b6bb4371b8d2
sabina <sabina.moreno.morlon@alumnos.upm.es>
2023-09-28 12:33:52 +0200

    Cambios sin subir

--- src/main/java/com/MicroserviciosTFG/GestorObras/GestorObrasApplication.java
+++ src/main/java/com/MicroserviciosTFG/GestorObras/GestorObrasApplication.java
@@ -2,8 +2,10 @@ package com.MicroserviciosTFG.GestorObras;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GestorObrasApplication {
 
 	public static void main(String[] args) {
